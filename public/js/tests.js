dd5aa6bd58af51724534a7f2e4859ec064bcf09d
Pablo Rosique Vidal <pablo.rosique@upct.es>
2024-02-19 08:15:39 +0100

    Add Menu component

--- public/js/tests.js
+++ public/js/tests.js
@@ -1,37 +1,20 @@
 
-import api from "../components/Api.js";
-import Form from "../components/Form.js";
-import nav from "../components/Navigation.js";
-import maps from "./iris/tests.js";
-import menus from "../data/menu.js";
-import i18n from "../i18n/langs.js";
-
-// Build tree menu as UL > Li > *
-function preorden(data, node, level) {
-    const children = data.filter(child => (child.padre == node.id)); // sub-menu items (children from node)
-    const label = `<span class="label-menu level-${level}">` + ((node.icono || "") + node.nombre) + "</span>"; // item menu label
-    if (children.length) {
-        var output = `<li class="item-menu level-${level} item-parent">`; // parent item menu (with children)
-        output += `<a href="${node.enlace}" class="link-menu level-${level}" title="${node.titulo}">${label}<i class="fas fa-caret-right icon-right"></i></a>`;
-        output += `<ul class="sub-menu level-${level + 1}">` + children.map(child => preorden(data, child, level + 1)).join("") + "</ul>";
-        return output + "</li>";
-    }
-    var output = `<li class="item-menu level-${level} item-leaf">`; // leaf item menu (item hoja)
-    output += `<a href="${node.enlace}" class="link-menu level-${level}" title="${node.titulo}">${label}</a>`;
-    output += children.map(child => preorden(data, child, level + 1)).join("");
-    return output + "</li>";
-}
+import api from "./components/Api.js";
+import Form from "./components/Form.js";
+import nav from "./components/Navigation.js";
+import menu from "./components/Menu.js";
+import menus from "./data/menus.js";
+import maps from "./xeco/iris/tests.js";
+import i18n from "./i18n/langs.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector("ul.menu");
-    menu.innerHTML = menus.sort((a, b) => (a.orden - b.orden))
-                            .filter(node => (!node.padre && (node.tipo == 1)))
-                            .map(node => preorden(menus, node, 1)).join("");
-    menu.slideIn();
+    const menuHTML = document.querySelector("ul.menu");
+    menuHTML.innerHTML = menu.html(menus.filter(node => (node.tipo == 1)).sort((a, b) => (a.orden - b.orden)));
+    menuHTML.slideIn();
     // toggle phone menu
     const menuToggleBtn = document.querySelector("#menu-toggle");
     menuToggleBtn.addEventListener("click", ev => {
-        menu.closest("nav").toggle("active");
+        menuHTML.closest("nav").toggle("active");
         menuToggleBtn.children.toggle();
     });
 
@@ -106,5 +89,5 @@ function index() {
     });
 }
 
-nav.addListener("/dist/views/maps.html", maps)
-    .addListener("/dist/views/index.html", index).addListener("/dist/views/", index);
+nav.addListener("/views/maps.html", maps)
+    .addListener("/views/index.html", index).addListener("/views/", index);
